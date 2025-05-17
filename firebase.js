1a0f48727359697b85dd08f1036a3f9291b3e2bc
icedmar <mbanimarine@gmail.com>
2025-05-17 14:47:14 +0300

    updated commit

--- firebase.js
+++ firebase.js
@@ -1,11 +1,17 @@
-// firebase.js
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./serviceAccountKey.json');
+const base64Key = process.env.FIREBASE_CONFIG_BASE64;
+
+if (!base64Key) {
+  throw new Error("FIREBASE_CONFIG_BASE64 env variable is missing.");
+}
+
+const decodedKey = Buffer.from(base64Key, 'base64').toString('utf-8');
+const serviceAccount = JSON.parse(decodedKey);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://boostbizz-217e3.firebaseio.com'
+  databaseURL: 'https://<your-project-id>.firebaseio.com' // Replace with your DB URL
 });
 
 const db = admin.firestore();
